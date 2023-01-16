79c4b20f7df541f86b9564a748f0c772ca6c0db0
Ilya Bayanov <i.p.bayanov@gmail.com>
2023-01-16 23:05:03 +0300

    feat: implement horizontal scale logic

--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,55 @@ import http from 'http';
 import cluster from 'cluster';
 import * as dotenv from 'dotenv';
 
-import { serverRequestHandler } from './serverRequestHandler';
+import {
+  serverRequestHandler,
+  balancerRequestHandler,
+  getWorkers,
+  IWorkerInitialMessage,
+} from './misc';
 import { runDatabase } from './database';
 
-dotenv.config();
-const PORT = Number(process.env.PORT) || 4000;
-// console.log(process.env.NODE_ENV_BALANCER);
-
-if (PORT <= 1) throw new Error('Invalid port');
-
-const databasePort = PORT - 1;
-runDatabase(databasePort);
-
 if (cluster.isPrimary) {
-  const server = http.createServer(
-    serverRequestHandler.bind(null, databasePort)
-  );
+  dotenv.config();
+  const mainPort = Number(process.env.PORT) || 4000;
+  const databasePort = mainPort - 1;
+  const withBalancer = process.env.NODE_ENV_BALANCER === 'with_balancer';
 
-  server.listen(PORT, () => console.log(`Server is listening on PORT ${PORT}`));
-  // for (let i = 1; i < 5; i++) {
-  //   const fork = cluster.fork({ PORT: PORT + i });
-  //   fork.on('online', () => fork.send({ databasePort }));
-  // }
+  if (mainPort <= 1) throw new Error('Invalid port');
+
+  let server: http.Server;
+
+  const database = cluster.fork({ PORT: databasePort });
+  database.once('online', () => {
+    const message: IWorkerInitialMessage = { runAs: 'database', databasePort };
+    database.send(message);
+  });
+
+  if (withBalancer) {
+    const workers = getWorkers(mainPort, databasePort);
+
+    server = http.createServer(balancerRequestHandler.bind(null, workers));
+  } else {
+    server = http.createServer(serverRequestHandler.bind(null, databasePort));
+  }
+
+  server.listen(mainPort, () =>
+    console.log(`Server is listening on http://localhost:${mainPort}`)
+  );
 } else {
-  // process.on('message', (msg) => {
-  //   console.log(process.env.PORT, msg);
-  // });
+  const workerPort = process.env.PORT;
+
+  process.once('message', ({ runAs, databasePort }: IWorkerInitialMessage) => {
+    if (runAs === 'database') runDatabase();
+
+    if (runAs === 'worker') {
+      const workerServer = http.createServer(
+        serverRequestHandler.bind(null, databasePort)
+      );
+
+      workerServer.listen(workerPort, () =>
+        console.log(`Worker is listening on http://localhost:${workerPort}`)
+      );
+    }
+  });
 }
