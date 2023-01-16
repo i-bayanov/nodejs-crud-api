cc092815448ec6ee5bcbe1852efcdaea6c9582c4
Ilya Bayanov <i.p.bayanov@gmail.com>
2023-01-16 22:57:43 +0300

    refactor: move serverRequestHandler to misc

--- src/misc/serverRequestHandler.ts
+++ src/misc/serverRequestHandler.ts
@@ -1,8 +1,9 @@
 import { IncomingMessage, ServerResponse } from 'http';
-import { Worker } from 'cluster';
 import { Socket } from 'net';
 
-import { validateAndParseBody } from './misc';
+import { validateAndParseBody } from './validateAndParseBody';
+import { getBodyFromRequest } from './getBodyFromRequest';
+import { databaseResponseHandler } from './databaseResponseHandler';
 
 export async function serverRequestHandler(
   databasePort: number,
@@ -11,12 +12,20 @@ export async function serverRequestHandler(
 ) {
   try {
     const { method, url } = request;
+
     const urlRE = /^\/api\/users(?:\/(?<userID>[^?#\/]+))?$/;
     const urlMatchResult = url?.match(urlRE);
 
     if (!urlMatchResult) {
       response.statusCode = 404;
       response.end('<h1>Error 404 - Not found</h1>');
+
+      return;
+    }
+
+    if (!method) {
+      response.statusCode = 400;
+      response.end('<h1>Error 400 - Invalid request method</h1>');
 
       return;
     }
@@ -32,7 +41,7 @@ export async function serverRequestHandler(
 
       if ('error' in body) {
         response.statusCode = 400;
-        response.end('<h1>Error 400 - Invalid request body</h1>');
+        response.end(`<h1>Error 400 - ${body.error}</h1>`);
 
         return;
       }
@@ -44,40 +53,13 @@ export async function serverRequestHandler(
       userID,
       body,
     };
-    databaseSocket.connect(databasePort, 'localhost');
+    databaseSocket.connect(databasePort);
     databaseSocket.write(JSON.stringify(databaseQuery));
     databaseSocket.on('data', (data) => {
-      databaseResponseHandler(response, data.toString());
+      databaseResponseHandler(method, response, data.toString());
     });
-
-    // database.on('message', databaseResponseHandler.bind(null, response));
-
-    // database.send(databaseQuery);
   } catch {
     response.statusCode = 500;
     response.end('<h1>Error 500 - Internal server error</h1>');
   }
-}
-
-async function getBodyFromRequest(request: IncomingMessage) {
-  return new Promise<string>((resolve, reject) => {
-    let requestBody = '';
-
-    request
-      .on('data', (chunk: string) => {
-        if (chunk) requestBody += chunk;
-      })
-      .on('end', () => {
-        resolve(requestBody);
-      })
-      .on('error', (error) => {
-        reject(error);
-      });
-  });
-}
-
-function databaseResponseHandler(response: ServerResponse, msg: any) {
-  response.statusCode = 200;
-  response.write(msg);
-  response.end();
 }
