0a9df42d5237d855a023218f55668b10d2065b2b
Ilya Bayanov <i.p.bayanov@gmail.com>
2023-01-16 22:59:35 +0300

    feat: add types ErrorMessages & IDatabaseResponse

--- src/@types/DatabaseQueries.d.ts
+++ src/@types/DatabaseQueries.d.ts
@@ -3,3 +3,10 @@ interface IQuery {
   userID: string | undefined;
   body: Omit<IUser, 'id'> | null;
 }
+
+type ErrorMessages =
+  | "Can't handle request"
+  | 'User not found'
+  | 'Invalid UserID';
+
+type IDatabaseResponse = IUser | IUser[] | { error: ErrorMessages };
